86967e8792584b1eebabbfa3d852537cefbfd85e
WannaMinPaing <paingwanna@gmail.com>
2025-02-03 15:38:22 +0630

    add dark and white logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -7,13 +7,23 @@ import DarkModeToggle from "../DarkModeToggle";
 const Navbar = () =>{
     return (  
         <div className="flex justify-between items-center bg-secondary dark:bg-dark py-[14px] mx-[80px] border-[1px] border-primary/90 mt-[20px] rounded-[10px]">
+            
             <Image
                 alt="Viatick"
                 src={images.logo}
                 width={100}
                 height={100}
-                className="w-[200px] h-[50px] ml-[20px]"
+                className="w-[200px] h-[50px] ml-[20px] block dark:hidden"
             />
+
+            <Image
+                alt="Viatick"
+                src={images.darkLogo}
+                width={100}
+                height={100}
+                className="w-[200px] h-[50px] ml-[20px] hidden dark:block"
+            />
+            
             <NavbarLinkContainer />
             <DarkModeToggle />
         </div> 
--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,6 @@
 const images = {
     logo: require('@/public/images/logo.png'),
+    darkLogo : require('@/public/images/dark_logo.png'),
 }
 
 export { images };
