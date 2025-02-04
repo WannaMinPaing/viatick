3e8f86aa3d115c86a8f2d6fdd0fad3d0e741a25e
WannaMinPaing <paingwanna@gmail.com>
2025-02-04 10:11:50 +0630

    add text animation

--- components/home/frequentlyAskedQuestion.tsx
+++ components/home/frequentlyAskedQuestion.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { FaChevronDown } from "react-icons/fa";
 import { questionAndAnswers } from "@/data/faq";
@@ -13,9 +13,9 @@ const FrequentlyAskedQuestion = () => {
     };
 
     return (
-        <div className="my-[50px] flex items-center justify-between mx-[20px]">
+        <div className="my-[50px] flex items-center justify-between mx-[20px] max-xl:flex-col-reverse">
     
-            <div className=" w-1/2 mr-[50px]">
+            <div className=" w-1/2 mr-[50px] max-xl:w-full max-xl:mt-[50px] max-xl:mx-[10px]">
                 <h3 className="mb-5 text-[calc(1rem_+_0.66vw)] font-bold !leading-[1.3] text-primary-500 dark:text-white-200 lg:mb-9 xl:text-[1.5rem]">
                     <span className="text-gradient dark:dark-text-gradient">The Facts, </span> you want to know
                 </h3>
@@ -23,7 +23,7 @@ const FrequentlyAskedQuestion = () => {
                 {questionAndAnswers.map((questionAndAnswer, index) => (
                     <div key={index}>
                         <button onClick={() => toggleFAQ(index)} className="pt-[5px] text-white flex justify-center items-center">
-                            <span className="dark:text-white text-blue font-bold font-SofiaSans">{questionAndAnswer.question}</span>
+                            <span className="dark:text-white text-blue font-bold font-SofiaSans text-left">{questionAndAnswer.question}</span>
                             <FaChevronDown 
                                 className={`w-[15px] h-[15px] ml-[10px] text-primary transition-transform duration-200 ${openFAQ === index ? "rotate-45" : ""}`} 
                             />
@@ -39,15 +39,15 @@ const FrequentlyAskedQuestion = () => {
                                 {questionAndAnswer.answer}
                             </div>
                         </motion.div>
-                        
+
                     </div>
                 ))}
             </div>
 
-            <div className="w-1/2 text-center">
-                <h3 className="mb-5 text-[calc(1rem_+_0.66vw)] font-bold !leading-[1.3] text-primary-500 dark:text-white-200 lg:mb-9 xl:text-[1.5rem]">
-                    About <span className="text-gradient dark:dark-text-gradient">Viatick, </span> 
-                </h3>
+            <div className="w-1/2 text-center max-xl:w-full">
+                <div className="mb-5 text-[calc(1rem_+_0.66vw)] font-bold !leading-[1.3] text-primary-500 dark:text-white-200 lg:mb-9 xl:text-[1.5rem]">
+                    <TypingEffect />
+                </div>
                 <div className="text-[18px]">
                     VIATICK Pte. Ltd. is an aIoT company that specializes in Artificial Intelligence through IoT. Our vision is to enable companies to use their own data to perform decisions that can make them more money, save more money, or discover insights that do both.
                 </div>
@@ -56,4 +56,37 @@ const FrequentlyAskedQuestion = () => {
     );
 };
 
+
+const TypingEffect = () => {
+    const text = "iatick,";
+    const [displayText, setDisplayText] = useState("");
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [counter, setCounter] = useState(0);
+  
+    useEffect(() => {
+      const typingInterval = setInterval(() => {
+        if (!isDeleting) {
+          if (counter < text.length) {
+            setDisplayText((prev) => prev + text[counter]);
+            setCounter((prev) => prev + 1);
+          } else {
+            setIsDeleting(true);
+          }
+        } else {
+          if (counter > 0) {
+            setDisplayText((prev) => prev.slice(0, -1));
+            setCounter((prev) => prev - 1);
+          } else {
+            setIsDeleting(false);
+          }
+        }
+      }, 300);
+  
+      return () => clearInterval(typingInterval);
+    }, [counter, isDeleting, text]);
+  
+    return <h1 className="text-primary-500 dark:text-white-200">About <span className="text-gradient dark:dark-text-gradient">V{displayText}</span></h1>;
+  };
+
+  
 export default FrequentlyAskedQuestion;
